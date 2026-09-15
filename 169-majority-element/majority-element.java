b836ab29ac858fe21769168b1b91ab74ea3e1399
Arjun Singh Jadon <arjun101jadone@gmail.com>
2026-09-15 10:27:19 +0530

    Time: 8 ms (39.81%) | Memory: 63.5 MB (7.20%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,51 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int value : nums) {
-            map.put(value, map.getOrDefault(value, 0) + 1);
-        }
-        for (int value : map.keySet()) {
-            if (map.get(value) > nums.length / 2) {
-                return value;
-            }
-        }
-        return -1;
+        //bruteforce approch
+    //     for(int i=0; i<nums.length; i++){
+    //         int frq=0;
+    //         for(int j=0; j<nums.length; j++){
+    //             if(nums[j]==nums[i]){
+    //                 frq++;
+    //             }
+    //         }
+    //         if(frq>nums.length/2){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }}
 
+    //optomize btureforce
+    Arrays.sort(nums);
+
+    int frq=1, ans = nums[0];
+    for(int i=1; i<nums.length; i++){
+        if(nums[i] == nums[i-1]){
+            frq++;
+        }
+        else{
+            frq=1;
+            ans = nums[i];
+        }
+
+        if(frq >nums.length/2 ){
+            return ans;
+        }
+    }
+    return ans;
     }
 }
+    
+        //Hashmap approch
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // for (int value : nums) {
+        //     map.put(value, map.getOrDefault(value, 0) + 1);
+        // }
+        // for (int value : map.keySet()) {
+        //     if (map.get(value) > nums.length / 2) {
+        //         return value;
+        //     }
+        // }
+        // return -1;
